79a135cdc4fe818a7402d2f2fa48bc538fb837c6
Jens von Bergmann <jens@mountainmath.ca>
2020-12-01 15:02:39 -0800

    health region level data

--- R/helpers.R
+++ R/helpers.R
@@ -27,3 +27,24 @@ compute_rolling_exp_fit <- function(r,window_width=7,min_obs=window_width-1,se=3
     mutate(low=slope-se*slope_e,high=slope+se*slope_e) %>%
     select(r=slope,low,high)
 }
+
+clean_missing_weekend_data <- function(tl){
+  zeros <- which(tl==0)
+  blocks <- which(!(zeros %in% (zeros+1)))
+  lengths <-lead(blocks)-blocks
+  lengths[length(lengths)]=length(zeros[zeros>=zeros[blocks[length(blocks)]]])
+  for (i in seq(1,length(blocks))){
+    b=zeros[blocks[i]]
+    l=lengths[i]
+    e=b+l
+    if (e>length(tl)) {
+      l=l-1
+      e=e-1
+    }
+    v=tl[e]/(l+1)
+    for (j in seq(0,l)) {
+      tl[b+j]=v
+    }
+  }
+  tl
+}
